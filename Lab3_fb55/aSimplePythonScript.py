18818950bab36301176beb08dd9da23d869eafc0
Chang-Du <41005878+Chang-Du@users.noreply.github.com>
2018-09-24 00:04:53 -0400

    Update aSimplePythonScript.py

--- Lab3_fb55/aSimplePythonScript.py
+++ Lab3_fb55/aSimplePythonScript.py
@@ -23,7 +23,7 @@ import sys
 # so sys.argv is a list with at least one element
 # with your name in input it will be a list of 2
 # if you add more than one word as argument it will give you an error as well
-if not len(sys.argv) == 2:
+if not len(sys.argv) == 2:#一个参数是2 两个参数是3
     print ("Invalid number of arguments. Run as: python  aSimplePythonScript.py YourNameHere")
     sys.exit()
 
